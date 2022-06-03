42e2c9b97fef119407fab69e6d2e39db1ea7fa27
CamilleBeaute <alyxis.c.johnson@gmail.com>
2022-06-03 09:38:28 -0400

    Add files via upload

---
+++ js/bucket.js
@@ -0,0 +1,99 @@
+$(document).ready(function(){
+  //DROPDOWN NAVIGATION
+  $(".nav-dropdown-btn").click(function() {
+    $(".nav-dropdown-content").animate({
+          height: "toggle",
+          opacity: "toggle"
+      }, "slow");
+  });
+
+  //IMAGE SLIDER
+  $('.hero-image-slider').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    speed: 1000,
+    pauseOnHover: false,
+    pauseOnFocus: false
+  });
+
+  //TEXT SLIDER
+  $('.hero-text-slider').slick({
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    arrows: false,
+    speed: 1000,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    fade: true,
+  });
+
+  //FORM LOGIC
+  $("#question-select").change(function() {
+    if ($(this).val() == "question-1") {
+      $('#question-1').show();
+      $('#question-2').hide();
+      $('#space').attr('value', 'Space 1');
+    } else if ($(this).val() == "question-2") {
+      $('#question-2').show();
+      $('#question-1').hide();
+      $('#space').attr('value', 'Space 2');
+    } else {
+      $('#question-1').hide();
+      $('#question-2').hide();
+      $('#space').attr('value', '');
+    }
+  });
+  $("#question-select").trigger("change");
+
+  //ADD PADDING FOR FIXED HEADER IN MOBILE
+  function calculateHeaderHeight(){
+    var headerHeight = $('header').outerHeight();
+    console.log(headerHeight);
+
+    if ($('.hero').length) {
+      $('.hero').css('padding-top', headerHeight);
+    } else if ($('.icon-profile').length) {
+      $('.icon-profile').css('padding-top', headerHeight);
+    } else if ($('.member-profile').length) {
+      $('.member-profile').css('padding-top', headerHeight);
+    } else {
+      $('.main-heading').css('padding-top', headerHeight);
+    }
+
+  }
+
+  //REMOVE PADDING WHEN RESIZING WINDOW WIDER THAN MOBILE
+  function undoHeaderHeight(){
+
+    if ($('.hero').length) {
+      $('.hero').css('padding-top', 0);
+    } else if ($('.icon-profile').length) {
+      $('.icon-profile').css('padding-top', 0);
+    } else if ($('.member-profile').length) {
+      $('.member-profile').css('padding-top', 0);
+    } else {
+      $('.main-heading').css('padding-top', 0);
+    }
+
+  }
+
+  if(699 >= $(window).width()) {
+    calculateHeaderHeight();
+  }
+
+  $(window).resize(function() {
+    if(699 >= $(window).width()) {
+      calculateHeaderHeight();
+    }
+  });
+
+  $(window).resize(function() {
+    if( $(window).width() >= 699) {
+      undoHeaderHeight();
+    }
+  });
+
+});
